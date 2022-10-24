80308357e6b48e2c82c57302cd5aa45457c08d1e
nolascaa <anolc93@gmail.com>
2022-10-24 03:29:00 -0600

    index.php start

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<header>
+  <style>
+      header {
+        text-align: center;
+        padding: 3px;
+        background-color: rgb(255, 255, 255);
+        color: rgb(43, 43, 43);
+      }
+  </style>
+
+  <h1>Pooh's Photo Repository</h1>
+</header>
