cb4b151d5429b2667d9c4dd9780a0c158b80a80d
nolascaa <anolc93@gmail.com>
2022-10-24 05:22:52 -0600

    Added navigation bar

---
+++ nav.php
@@ -0,0 +1,14 @@
+<nav>
+        <style>
+            nav {
+              text-align: center;
+              padding: 20px;
+              font-size: 120%;
+              background-color: rgb(255, 255, 255);
+              color: rgb(255, 255, 255);
+            }
+        </style>
+        <a href="index.php">Home</a>
+        <a href="upload.php">Upload</a>
+        <a href="album.php">Album</a>
+    </nav>
